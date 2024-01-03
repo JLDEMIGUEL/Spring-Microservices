addcd758568f8fe2063d972c80f817f64eba3064
jldemiguel <jldmp2001@gmail.com>
2024-01-03 18:35:04 +0100

    Added Cards and Loans microservices

---
+++ loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -0,0 +1,39 @@
+package com.eazybytes.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "EazyBank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Jose Luis De Miguel",
+                        email = "jldmp2001@gmail.com",
+                        url = "https://github.com/JLDEMIGUEL/"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/JLDEMIGUEL/"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Loans microservice REST API Documentation",
+                url = "https://github.com/JLDEMIGUEL/"
+        )
+)
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+}
