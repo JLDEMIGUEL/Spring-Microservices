89002c8f99b5080fe497ec9a6747cde895739ac6
jldemiguel <jldmp2001@gmail.com>
2024-01-08 18:58:26 +0100

    Enable actuator endpoints in all microservices

--- cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,15 @@
 package com.eazybytes.cards.dto;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public final class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,15 @@
 package com.eazybytes.loans.dto;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public final class LoansContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
