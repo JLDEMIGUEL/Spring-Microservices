c78ea9e04e16b434b6ced12b9e008d042e38da42
jldemiguel <jldmp2001@gmail.com>
2024-01-12 19:22:09 +0100

    Added circuit breaker patter between accounts and cards and loans

--- accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "loans", path = "/api")
+@FeignClient(value = "loans", path = "/api", fallback = LoansFallback.class)
 public interface LoansFeignClient {
 
     @GetMapping(value = "/fetch", consumes = "application/json")
--- accounts/src/main/java/com/eazybytes/accounts/service/impl/ICustomerServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/ICustomerServiceImpl.java
@@ -43,10 +43,14 @@ public class ICustomerServiceImpl implements ICustomerService {
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(account, new AccountsDto()));
 
         ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
-        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        if (loansDtoResponseEntity != null) {
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
 
         ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
-        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        if (cardsDtoResponseEntity != null) {
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
         return customerDetailsDto;
     }
 }
